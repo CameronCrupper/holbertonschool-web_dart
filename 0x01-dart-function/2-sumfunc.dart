e703657796e4ac383c13871f6e985419b66d7f27
Cameron Crupper <guitardude20795@yahoo.com>
2023-02-22 10:00:28 -0600

    add and sub and print lines

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,12 @@
+int add(int a, int b)
+{
+  return a + b;
+}
+int sub(int a, int b)
+{
+  return a - b;
+}
+String showFunc(int a, int b)
+{
+  return('Add ${a} + ${b}' + '\n' + 'Sub ${a} - ${b}');
+}
